7684509b957d872042adfe1f7548f0400bbf05ac
liuqinghua96 <865708365@qq.com>
2019-04-24 18:29:14 +0800

    权限列表完成

--- src/components/rights/Rights.vue
+++ src/components/rights/Rights.vue
@@ -1,12 +1,42 @@
 <template>
-  <h1>权限列表</h1>
+  <div class="rights_container">
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{path: '/welcome'}">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片 -->
+    <el-card class="box-card">
+      <el-table :data="rightsList"
+                stripe
+                height="400"
+                style="width: 100%">
+        <el-table-column type="index"
+                         width="50">
+        </el-table-column>
+        <el-table-column property="authName"
+                         label="权限名称">
+        </el-table-column>
+        <el-table-column property="path"
+                         label="路径">
+        </el-table-column>
+        <el-table-column label="权限等级">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">一级权限</el-tag>
+            <el-tag v-if="scope.row.level === '1'" type="success">二级权限</el-tag>
+            <el-tag v-if="scope.row.level === '2'" type="warning">三级权限</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 <script>
-import Mixin from './Roles_Mixin'
+import Mixin from './Rights_Mixin'
 export default {
   mixins: [Mixin]
 }
 </script>
 <style scoped>
-
 </style>
--- src/components/rights/Rights_Mixin.js
+++ src/components/rights/Rights_Mixin.js
@@ -1,3 +1,18 @@
 export default {
-  
+  name: 'Rights',
+  data () {
+    return {
+      rightsList: []
+    }
+  },
+  methods: {
+    async getData () {
+      const {data: {data, meta}} = await this.$http.get(`rights/list`)
+      if (meta.status !== 200) return this.$message.error(meta.msg)
+      this.rightsList = data
+    }
+  },
+  mounted () {
+    this.getData()
+  }
 }
